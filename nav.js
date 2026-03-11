e4711bbdc23f1a9129d7dab93e7f1af649fabfb8
durchdenwaldjulian-svg <durchdenwald.julian@googlemail.com>
2026-03-11 09:53:14 +0100

    Add files via upload

--- nav.js
+++ nav.js
@@ -161,7 +161,7 @@
         ];
 
         var h = '<div class="mobile-menu-box">';
-        h += '<div class="mobile-menu-header"><h2><img src="Logo.svg" alt="BienenPlan" style="height:80px;vertical-align:middle;margin-right:.5rem"> Navigation</h2>';
+        h += '<div class="mobile-menu-header"><h2><img src="Logo.svg" alt="BienenPlan" style="height:120px;vertical-align:middle;margin-right:.5rem"> Navigation</h2>';
         h += '<button class="mobile-menu-close" onclick="mobileMenuClose()">✕</button></div>';
 
         groups.forEach(function(g) {
